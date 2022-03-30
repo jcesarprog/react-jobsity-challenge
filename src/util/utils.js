ffcbdd3e568104397ab13a50d950b09b54818f1e
Julio Teixeira <jcesarprog@gmail.com>
2022-03-30 10:45:53 -0300

    added: test for the algorithm of the days from a month

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,7 +1,6 @@
 import dayjs from "dayjs";
 
 export function getMonth(month = dayjs().month()) {
-    // month = Math.floor(month);
     const year = dayjs().year();
     const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day();
     let currentMonthCount = 0 - firstDayOfTheMonth;
