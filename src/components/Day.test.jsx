4117efd4e853a1b1ccad3d217e390c6c048c7ff1
Julio Teixeira <jcesarprog@gmail.com>
2022-03-31 00:29:57 -0300

    clean: cleaned not used imports

--- src/components/Day.test.jsx
+++ src/components/Day.test.jsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { AppContextProvider } from "../contexts/AppContext";
 import { getMonth } from "../util/utils";
 import { Day } from "./Day";
